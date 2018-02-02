4e6085c3ec97acf6cf1b8a65b2de3b8d91c9c1e4
Dale Herring <dalejherring@gmail.com>
2018-02-02 08:21:04 -0600

    Add files via upload

---
+++ classes/ScoreRetrieval.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ScoreRetrievalApp
+{
+    partial class ScoreRetrievalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InstLbl = new System.Windows.Forms.Label();
+            this.ErrorLbl = new System.Windows.Forms.Label();
+            this.DispNewWriteBox = new System.Windows.Forms.ListBox();
+            this.RewriteBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InstLbl
+            // 
+            this.InstLbl.AutoSize = true;
+            this.InstLbl.Location = new System.Drawing.Point(20, 20);
+            this.InstLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.InstLbl.Name = "InstLbl";
+            this.InstLbl.Size = new System.Drawing.Size(303, 20);
+            this.InstLbl.TabIndex = 0;
+            this.InstLbl.Text = "Click the button to write scores to the box.";
+            // 
+            // ErrorLbl
+            // 
+            this.ErrorLbl.AutoSize = true;
+            this.ErrorLbl.Location = new System.Drawing.Point(20, 317);
+            this.ErrorLbl.Name = "ErrorLbl";
+            this.ErrorLbl.Size = new System.Drawing.Size(0, 20);
+            this.ErrorLbl.TabIndex = 0;
+            this.ErrorLbl.Visible = false;
+            // 
+            // DispNewWriteBox
+            // 
+            this.DispNewWriteBox.Enabled = false;
+            this.DispNewWriteBox.FormattingEnabled = true;
+            this.DispNewWriteBox.ItemHeight = 20;
+            this.DispNewWriteBox.Location = new System.Drawing.Point(24, 71);
+            this.DispNewWriteBox.Name = "DispNewWriteBox";
+            this.DispNewWriteBox.Size = new System.Drawing.Size(299, 164);
+            this.DispNewWriteBox.TabIndex = 1;
+            // 
+            // RewriteBtn
+            // 
+            this.RewriteBtn.Location = new System.Drawing.Point(24, 262);
+            this.RewriteBtn.Name = "RewriteBtn";
+            this.RewriteBtn.Size = new System.Drawing.Size(75, 46);
+            this.RewriteBtn.TabIndex = 1;
+            this.RewriteBtn.Text = "&Rewrite";
+            this.RewriteBtn.UseVisualStyleBackColor = true;
+            this.RewriteBtn.Click += new System.EventHandler(this.RewriteBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(235, 262);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 46);
+            this.ClearBtn.TabIndex = 2;
+            this.ClearBtn.Text = "&Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitBtn.Location = new System.Drawing.Point(449, 262);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 46);
+            this.ExitBtn.TabIndex = 3;
+            this.ExitBtn.Text = "E&xit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // ScoreRetrievalForm
+            // 
+            this.AcceptButton = this.RewriteBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ExitBtn;
+            this.ClientSize = new System.Drawing.Size(549, 346);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.RewriteBtn);
+            this.Controls.Add(this.DispNewWriteBox);
+            this.Controls.Add(this.ErrorLbl);
+            this.Controls.Add(this.InstLbl);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ScoreRetrievalForm";
+            this.Text = "Score Averages";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ScoreRetrievalForm_FormClosing);
+            this.Load += new System.EventHandler(this.ScoreRetrievalForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InstLbl;
+        private System.Windows.Forms.Label ErrorLbl;
+        private System.Windows.Forms.ListBox DispNewWriteBox;
+        private System.Windows.Forms.Button RewriteBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
---
+++ classes/ScoreRetrieval.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScoreRetrievalApp
+{
+    public partial class ScoreRetrievalForm : Form
+    {
+        private StreamReader inFile;
+        private StreamWriter outFile;
+        string[] line1, line2, line3, line4, line5, line6, line7, line8;
+        string inValue;
+        int caseCnt, avg1, avg2, avg3, avg4, avg5, avg6, avg7, avg8;
+
+        private void ScoreRetrievalForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseMth();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            CloseMth();
+            Close();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            DispNewWriteBox.Items.Clear();
+            ErrorLbl.Hide();
+            ErrorLbl.Text = " ";
+            RewriteBtn.Enabled = true;
+        }
+
+        private void RewriteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                while ((inValue = inFile.ReadLine()) != null)
+                {
+                    switch (caseCnt)
+                    {
+                        case 1:
+                            line1 = inValue.Split(' ');
+                            break;
+                        case 2:
+                            line2 = inValue.Split(' ');
+                            break;
+                        case 3:
+                            line3 = inValue.Split(' ');
+                            break;
+                        case 4:
+                            line4 = inValue.Split(' ');
+                            break;
+                        case 5:
+                            line5 = inValue.Split(' ');
+                            break;
+                        case 6:
+                            line6 = inValue.Split(' ');
+                            break;
+                        case 7:
+                            line7 = inValue.Split(' ');
+                            break;
+                        case 8:
+                            line8 = inValue.Split(' ');
+                            break;
+                        default:
+                            ErrorLbl.Text = "Error";
+                            break;
+                    }//end switch
+
+                    caseCnt++;
+
+                } //end while
+
+                avg1 = (int.Parse(line1[2]) + int.Parse(line1[3]) + int.Parse(line1[4])) / 3;
+                avg2 = (int.Parse(line2[2]) + int.Parse(line2[3]) + int.Parse(line2[4])) / 3;
+                avg3 = (int.Parse(line3[2]) + int.Parse(line3[3]) + int.Parse(line3[4])) / 3;
+                avg4 = (int.Parse(line4[2]) + int.Parse(line4[3]) + int.Parse(line4[4])) / 3;
+                avg5 = (int.Parse(line5[2]) + int.Parse(line5[3]) + int.Parse(line5[4])) / 3;
+                avg6 = (int.Parse(line6[2]) + int.Parse(line6[3]) + int.Parse(line6[4])) / 3;
+                avg7 = (int.Parse(line7[2]) + int.Parse(line7[3]) + int.Parse(line7[4])) / 3;
+                avg8 = (int.Parse(line8[2]) + int.Parse(line8[3]) + int.Parse(line8[4])) / 3;
+
+                outFile.WriteLine(line1[0] + " " + line1[1] + " " + avg1);
+                outFile.WriteLine(line2[0] + " " + line2[1] + " " + avg2);
+                outFile.WriteLine(line3[0] + " " + line3[1] + " " + avg3);
+                outFile.WriteLine(line4[0] + " " + line4[1] + " " + avg4);
+                outFile.WriteLine(line5[0] + " " + line5[1] + " " + avg5);
+                outFile.WriteLine(line6[0] + " " + line6[1] + " " + avg6);
+                outFile.WriteLine(line7[0] + " " + line7[1] + " " + avg7);
+                outFile.WriteLine(line8[0] + " " + line8[1] + " " + avg8);
+
+                DispNewWriteBox.Items.Add(line1[0] + " " + line1[1] + " " + avg1);
+                DispNewWriteBox.Items.Add(line2[0] + " " + line2[1] + " " + avg2);
+                DispNewWriteBox.Items.Add(line3[0] + " " + line3[1] + " " + avg3);
+                DispNewWriteBox.Items.Add(line4[0] + " " + line4[1] + " " + avg4);
+                DispNewWriteBox.Items.Add(line5[0] + " " + line5[1] + " " + avg5);
+                DispNewWriteBox.Items.Add(line6[0] + " " + line6[1] + " " + avg6);
+                DispNewWriteBox.Items.Add(line7[0] + " " + line7[1] + " " + avg7);
+                DispNewWriteBox.Items.Add(line8[0] + " " + line8[1] + " " + avg8);
+
+                RewriteBtn.Enabled = false;
+            }//end try
+            catch (FormatException frmX)
+            {
+                ErrorLbl.Text = frmX.Message;
+                ErrorLbl.Show();
+            }
+            catch (ArithmeticException aX)
+            {
+                ErrorLbl.Text = aX.Message;
+                ErrorLbl.Show();
+            }
+            catch (ArrayTypeMismatchException atX)
+            {
+                ErrorLbl.Text = atX.Message;
+                ErrorLbl.Show();
+            }
+            catch (IndexOutOfRangeException iorX)
+            {
+                ErrorLbl.Text = iorX.Message;
+                ErrorLbl.Show();
+            }
+            catch (Exception eX)
+            {
+                ErrorLbl.Text = eX.Message;
+                ErrorLbl.Show();
+            }
+        }//end rewrite btn click
+
+        public ScoreRetrievalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ScoreRetrievalForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists("StoredScores.txt"))
+                {
+                    inFile = new StreamReader("StoredScores.txt");  
+                    outFile = new StreamWriter("AvgScores.txt");
+                } //end if
+                
+                line1 = new string[5];
+                line2 = new string[5];
+                line3 = new string[5];
+                line4 = new string[5];
+                line5 = new string[5];
+                line6 = new string[5];
+                line7 = new string[5];
+                line8 = new string[5];
+                caseCnt = 1;
+            }//end try
+            catch (FileNotFoundException fX)
+            {
+                ErrorLbl.Text = fX.Message;
+                ErrorLbl.Show();
+            }//end catch
+            catch (Exception X)
+            {
+                ErrorLbl.Text = X.Message;
+                ErrorLbl.Show();
+            }//end catch
+        }//end formload
+
+        private void CloseMth()
+        {
+            try
+            {
+                inFile.Close();
+                outFile.Close();         
+            }//end try
+            catch (IOException ioX)
+            {
+                ErrorLbl.Text = ioX.Message;
+                ErrorLbl.Show();
+            }
+            catch (Exception xX)
+            {
+                ErrorLbl.Text = xX.Message;
+                ErrorLbl.Show();
+            }
+        }//end close
+
+
+    }//end class
+}//end namespace
